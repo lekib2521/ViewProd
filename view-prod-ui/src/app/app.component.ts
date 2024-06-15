506b9e3a96d75dad08145297a80fbd40259209ba
Lekshmi Priya B <lekshmipriya.b.2023@iimu.ac.in>
2024-06-15 12:14:09 +0530

    adding tab name and icon

--- view-prod-ui/src/app/app.component.ts
+++ view-prod-ui/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,8 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  constructor(private titleService: Title) {
+    this.titleService.setTitle("ViewProd");
+  }
   title = 'view-prod-ui';
 }
